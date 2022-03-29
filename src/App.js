53f3af73ce34848f9630b3b711fdd7b7a3216a38
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-03-29 14:46:55 -0300

    chore: setup dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <>Oi</>
-  );
-}
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import Series from './pages/Series'
+
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+          <li>
+            <Link to="/series">TV Series</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/movies' element={<Movies />} />
+        <Route path="/series" element={<Series />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  </Router>
+)
 
 export default App;
