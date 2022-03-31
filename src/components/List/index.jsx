2c54756e790ada60bc1a8ac5c7cea558994e6d07
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-03-30 21:50:58 -0300

    feat: add List component

---
+++ src/components/List/index.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+import * as S from './styles';
+
+const List = ({ children }) => <S.List>{children}</S.List>;
+export const ListItem = ({ children }) => <S.ListItem>{children}</S.ListItem>;
+
+List.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+ListItem.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default List;
---
+++ src/components/List/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  margin-top: 8px;
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
+  padding: 16px;
+`;
